594db8f72c71c83fe002b09f47bfb2291de229e6
github username <github email address>
2019-08-06 15:54:04 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+# ### 3. Define a `Person` in `lib/person.rb`
+# Open `lib/person.rb` and add a class definition for a `Person` class.
+
+class Person
+end
+
+# ### 4. Make 2 people in `lib/person.rb`
+# Under your `Person` class definition, create two people in local variables, `adele_goldberg` and `alan_kay`
+# When you're done, submit the lab with `learn submit`.
+
+adele_goldberg = Person.new
+
+alan_kay = Person.new
